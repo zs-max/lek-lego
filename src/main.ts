739ce9787b16d053f0ad20ae7f756d5028c68334
lwk <lk_demo@163.com>
2025-07-13 22:16:51 +0800

    安装ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Antd)
+app.mount('#app')
