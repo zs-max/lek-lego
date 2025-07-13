4763b2fb001c4dc5d101e9d424f66e97ff1662c7
lwk <lk_demo@163.com>
2025-07-13 22:35:52 +0800

    初始化页面和组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <template>
-  <hello msg="hello"></hello>
-  <h1>慕课乐高标准模版</h1>
-  <a-button type="primary">primary</a-button>
-  <ul>
-    <li>开箱即用</li>
-    <li>typescript</li>
-    <li>Vue3</li>
-    <li>支持 tsx</li>
-    <li>eslint</li>
-    <li>简单易用可扩展</li>
-  </ul>
+    <home />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { defineComponent } from 'vue';
-import Hello from "@/components/Hello";
-
-export default defineComponent({
-  name: 'App',
-  components:{
-    Hello
-  }
-});
+import Home from './views/Home.vue'
+import Editor from './views/Editor.vue'
 </script>
 
 <style>
