30974f8400d67c8c5ed012c74b3d80524d7fa372
lwk <lk_demo@163.com>
2025-08-01 06:50:04 +0800

    feat(上传组件)：编写上传组件的基本流程

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="app-container">
+    <uploader action="http://local.test:7001/api/upload" />
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Uploader from '@/components/Uploader.vue'
 export default defineComponent({
   name: 'App',
+  components:{
+    Uploader
+  }
 });
 </script>
 
---
+++ src/components/Uploader.vue
@@ -0,0 +1,64 @@
+<template>
+   <div class="file-upload">
+     <button @click="triggerUpload">
+       <span v-if="fileStatus === 'error'">上传失败</span>
+       <span v-else-if="fileStatus === 'loading'">上传中</span>
+       <span v-else-if="fileStatus === 'success'">上传成功</span>
+       <span v-else>点击上传</span>
+     </button>
+     <input ref="fileInput" type="file" :style="{display:'none'}" @change="onFileChange" />
+   </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue'
+import axios from "axios";
+type UploadStatus = 'ready'|'loading'|'success'|'error'
+export default defineComponent({
+  props: {
+    action: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props,context){
+    const fileInput = ref<null|HTMLInputElement>(null)
+    const fileStatus = ref<UploadStatus>('ready')
+    const triggerUpload = ()=>{
+      fileInput.value?.click()
+    }
+    const onFileChange = (e:Event)=>{
+      const target = e.target as HTMLInputElement
+      const files = target.files
+      if(files){
+        const uploadFile = files[0]
+        const formData = new FormData()
+        formData.append(uploadFile.name,uploadFile)
+        // 'https://local.test:7001/api/upload's
+        axios.post(props.action,formData,{
+          headers:{
+            'Content-Type':'multipart/form-data'
+          }
+        }).then(resp=>{
+          debugger
+          fileStatus.value = 'success'
+        }).catch(e=>{
+          debugger
+          fileStatus.value = 'error'
+        })
+      }
+    }
+    return {
+      fileInput,
+      triggerUpload,
+      fileStatus,
+      onFileChange
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
